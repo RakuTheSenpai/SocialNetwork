daeccbb9c7083ecb551de7341a85034c984ec3f8
rakuthesenpai <raul.r.h.rosas@gmail.com>
2019-05-17 16:51:45 -0700

    Minor Changes

---
+++ js/verify.spec.js
@@ -0,0 +1,16 @@
+'use strict';
+
+describe('myApp.verify module', function() {
+
+  beforeEach(module('myApp.verify'));
+
+  describe('verify controller', function(){
+
+    it('should ....', inject(function($controller) {
+      //spec body
+      var VerifyCtrl = $controller('VerifyCtrl');
+      expect(VerifyCtrl).toBeDefined();
+    }));
+
+  });
+});

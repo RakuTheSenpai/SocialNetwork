5aa6efb4a52486c9313ac9da4826fa23044d93e1
brendapaz1 <brendapaz1771@gmail.com>
2019-05-21 14:21:27 -0700

    Upload-Vista

--- js/upload.js
+++ js/upload.js
@@ -10,3 +10,30 @@ angular.module('myApp.upload', ['ngRoute'])
     .controller('UploadCtrl', ['$scope', function($scope) {
         
     }]);;
+FilePond.registerPlugin(
+    FilePondPluginFileEncode,
+    FilePondPluginFileValidateType,
+    FilePondPluginImageExifOrientation,
+    FilePondPluginImagePreview,
+    FilePondPluginImageCrop,
+    FilePondPluginImageResize,
+    FilePondPluginImageTransform
+);
+
+FilePond.create(document.querySelector('input'),
+{
+imagePreviewHeight: 320,
+imageCropAspectRatio: '1:1',
+imageResizeTargetWidth: 360,
+imageResizeTargetHeight: 360,
+styleLoadIndicatorPosition: 'center bottom',
+styleButtonRemoveItemPosition: 'center bottom'
+}
+);
+//   $('.chips').chips({
+//     placeholder: 'Enter a tag',
+//     secondaryPlaceholder: '+Tag',
+//     minLength: 1
+//   });
+
+

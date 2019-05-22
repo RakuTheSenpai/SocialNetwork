dee4f90d1d866a5de37da72dd35d20902a9b7c89
rakuthesenpai <raul.r.h.rosas@gmail.com>
2019-05-21 20:50:23 -0700

    Can now upload files, missing s3

--- js/upload.js
+++ js/upload.js
@@ -2,38 +2,38 @@
 
 angular.module('myApp.upload', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
+    .config(['$routeProvider', function ($routeProvider) {
         $routeProvider.when('/upload', {
             templateUrl: 'views/upload.html',
         });
     }])
-    .controller('UploadCtrl', ['$scope', function($scope) {
-        
-    }]);;
-FilePond.registerPlugin(
-    FilePondPluginFileEncode,
-    FilePondPluginFileValidateType,
-    FilePondPluginImageExifOrientation,
-    FilePondPluginImagePreview,
-    FilePondPluginImageCrop,
-    FilePondPluginImageResize,
-    FilePondPluginImageTransform
-);
+    .controller('UploadCtrl', ['$scope', function ($scope) {
+        FilePond.registerPlugin(
+            FilePondPluginFileEncode,
+            FilePondPluginFileValidateType,
+            FilePondPluginImageExifOrientation,
+            FilePondPluginImagePreview,
+            FilePondPluginImageCrop,
+            FilePondPluginImageResize,
+            FilePondPluginImageTransform
+        );
 
-FilePond.create(document.querySelector('input'),
-{
-imagePreviewHeight: 320,
-imageCropAspectRatio: '1:1',
-imageResizeTargetWidth: 360,
-imageResizeTargetHeight: 360,
-styleLoadIndicatorPosition: 'center bottom',
-styleButtonRemoveItemPosition: 'center bottom'
-}
-);
-//   $('.chips').chips({
-//     placeholder: 'Enter a tag',
-//     secondaryPlaceholder: '+Tag',
-//     minLength: 1
-//   });
+        FilePond.create(document.querySelector('input[type="file"]'),
+            {
+                imagePreviewHeight: 320,
+                imageCropAspectRatio: '1:1',
+                imageResizeTargetWidth: 360,
+                imageResizeTargetHeight: 360,
+                styleLoadIndicatorPosition: 'center bottom',
+                styleButtonRemoveItemPosition: 'center bottom'
+            }
+        );
+        const pond = document.querySelector('.filepond--root');
+        pond.addEventListener('FilePond:addfile', e => {
+            var file = e.detail.file.file;
+            var filename = e.detail.file.filename;
+            
+        });
 
+    }]);
 

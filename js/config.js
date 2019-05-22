fe84afaadbe7154eea4cbc9c2c1863ba1da0ff97
rakuthesenpai <raul.r.h.rosas@gmail.com>
2019-05-21 21:35:47 -0700

    Fixes

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_7gw1ZlZNJ', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '7neokuukstum4f0rohs6105ns', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_O9FULn1nA', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '2h4h9irb64mtknv88m9v2rf91s', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://wu7hx994df.execute-api.us-east-1.amazonaws.com/First' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://fxscqzitb6.execute-api.us-east-1.amazonaws.com/UnaVez' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
--- js/upload.js
+++ js/upload.js
@@ -7,7 +7,25 @@ angular.module('myApp.upload', ['ngRoute'])
             templateUrl: 'views/upload.html',
         });
     }])
-    .controller('UploadCtrl', ['$scope', function ($scope) {
+    .controller('UploadCtrl', ['$scope', '$http', function ($scope, $http) {
+        var poolData = {
+            UserPoolId: _config.cognito.userPoolId,
+            ClientId: _config.cognito.userPoolClientId
+        };
+        var userPool;
+        userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
+
+        var authToken;
+        window.authToken.then(function setAuthToken(token) {
+            if (token) {
+                authToken = token;
+            } else {
+                window.location.href = '#!/login';
+            }
+        }).catch(function handleTokenError(error) {
+            Swal.fire(error);
+            window.location.href = '#!/login';
+        });
         FilePond.registerPlugin(
             FilePondPluginFileEncode,
             FilePondPluginFileValidateType,
@@ -30,10 +48,28 @@ angular.module('myApp.upload', ['ngRoute'])
         );
         const pond = document.querySelector('.filepond--root');
         pond.addEventListener('FilePond:addfile', e => {
+            console.log(authToken);
             var file = e.detail.file.file;
             var filename = e.detail.file.filename;
-            
-        });
+            var req = {
+                method: 'POST',
+                url: _config.api.invokeUrl + '/getusuariomema',
+                headers: {
+                    Authorization: authToken
+                },
+                data: {
 
+                }
+            };
+            $http(req).then(function successCallback(response) {
+                console.log('Success');
+                var user = response.data.Items;
+                
+            }, function errorCallback(response) {
+                console.error('Error');
+                console.error(response);
+            });
+
+        });
     }]);
 

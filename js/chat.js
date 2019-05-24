13d744eb71ef85e95305f7068bdbf377439fceac
adrian <thechouzanone@gmail.com>
2019-05-24 12:07:15 -0700

    Wat

--- js/chat.js
+++ js/chat.js
@@ -7,7 +7,7 @@ angular.module('myApp.chat', ['ngRoute',"pubnub.angular.service"])
         });
     }])
     .controller('ChatCtrl', function($scope, Pubnub) {
-
+        
         // Listening to the callbacks
         $scope.$on(Pubnub.getMessageEventNameFor($scope.channel), function (ngEvent, m) {
             $scope.$apply(function () {
@@ -35,19 +35,46 @@ angular.module('myApp.chat', ['ngRoute',"pubnub.angular.service"])
              $scope.messageContent = '';
          }
 
+
+        $scope.initChat = function(){
+            Pubnub.init({
+                publish_key: 'pub-c-50877502-ae22-45dc-a606-d8f589fa76db',
+                subscribe_key: 'sub-c-157f2f30-7c0e-11e9-a950-f249fab64e16',
+                uuid: $scope.user
+            });
+            
+            $scope.channel = 'messages-channel';
+            Pubnub.subscribe({
+                channel: $scope.channel,
+                triggerEvents: ['callback']
+            });
+            Pubnub.history({
+                channel: $scope.channel,
+                callback: function(m) {
+                    console.log(m);
+                    angular.extend($scope.messages, m[0]); //This empties messages.
+                },
+                count: 20
+            });
+        }
+
         $scope.changeContact = function(contact){
             // Pubnub.unsubscribe({
             //     channel: $scope.channel
-            // }); //Esto me regresa un error raro :(
+            // }); //Esto me regresa un error raro de CORB :(
+            // console.log("Desuscrito :(")
+            $scope.channel = contact;
             $scope.currentContact = contact;
             Pubnub.subscribe({
                 channel: $scope.channel,
                 triggerEvents: ['callback']
             });
+            // console.log("Suscrito :)")
             console.log(contact);
             Pubnub.history({
                 channel: $scope.channel,
                 callback: function(m) {
+                    console.log(m);
                     angular.extend($scope.messages, m[0]); //This empties messages.
                 },
                 count: 20
@@ -55,22 +82,18 @@ angular.module('myApp.chat', ['ngRoute',"pubnub.angular.service"])
         }
 
         $scope.user = "Chouza"; //Should be the current user, not the current contact from the chat.
-        Pubnub.init({
-            publish_key: 'pub-c-50877502-ae22-45dc-a606-d8f589fa76db',
-            subscribe_key: 'sub-c-157f2f30-7c0e-11e9-a950-f249fab64e16',
-            uuid: $scope.user
-        });
-
-        $scope.channel = 'messages-channel';
-        $scope.currentContact = ""; 
+        $scope.channel = "";
+        $scope.currentContact = "";
         $scope.contacts = [
             "Raku",
             "Jkks",
             "Brenda",
             "Sergio",
-            "Sachy"
+            "Sachy",
+            "messages-channel"
         ];
         $scope.messages = [];
-        $scope.changeContact($scope.contacts[0]);
+        $scope.initChat();
+        // $scope.changeContact($scope.contacts[0]);
     });
     

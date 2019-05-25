2e41c2db25ec68bd0b5267f7d737016d94f814f1
rakuthesenpai <raul.r.h.rosas@gmail.com>
2019-05-24 19:34:51 -0700

    Added chat functionality

--- js/chat.js
+++ js/chat.js
@@ -1,44 +1,46 @@
 'use strict';
 // require(["underscore"])
-angular.module('myApp.chat', ['ngRoute',"pubnub.angular.service"])
-    .config(['$routeProvider', function($routeProvider) {
+angular.module('myApp.chat', ['ngRoute', "pubnub.angular.service"])
+    .config(['$routeProvider', function ($routeProvider) {
         $routeProvider.when('/chat', {
             templateUrl: 'views/chat.html',
         });
     }])
-    .controller('ChatCtrl', ['$scope', '$http', 'Pubnub', function($scope, $http, Pubnub) {
+    .controller('ChatCtrl', ['$scope', '$http', 'Pubnub', function ($scope, $http, Pubnub) {
+        $scope.currentContact = "";
         // Listening to the callbacks
         $scope.$on(Pubnub.getMessageEventNameFor($scope.channel), function (ngEvent, m) {
-            $scope.$apply(function () { 
+            $scope.$apply(function () {
                 $scope.currentMessages.push(m);
             });
         });
-
-        $scope.sendMessage = function() {
+        $scope.sendMessage = function () {
             // Don't send an empty message 
             if (!$scope.messageContent || $scope.messageContent === '') {
-                 return;
-             }
-             Pubnub.publish({
-                 channel: $scope.channel,
-                 message: {
-                     content: $scope.messageContent,
-                     sender_uuid: $scope.user,
-                     date: new Date()
-                 }, 
-                 callback: function(m) {
-                     console.log("El siguiente mensaje se publico a "+$scope.channel);
-                     console.log(m);
-                 }
-             });
-             // Reset the messageContent input
-             $scope.messageContent = '';
+                return;
+            }
+            Pubnub.publish({
+                channel: $scope.channel,
+                message: {
+                    content: $scope.messageContent,
+                    sender_uuid: $scope.user,
+                    date: new Date()
+                },
+                callback: function (m) {
+                    console.log("El siguiente mensaje se publico a " + $scope.channel);
+                    console.log(m);
+                }
+            });
+            // Reset the messageContent input
+            $scope.messageContent = '';
         }
 
-        $scope.changeContact = function(contact){
-            if($scope.user > contact){
+        $scope.changeContact = function (contact) {
+            $scope.currentContact = contact;
+            console.log($scope.currentContact);
+            if ($scope.user > contact) {
                 $scope.channel = $scope.user + "|" + contact;
-            }else{
+            } else {
                 $scope.channel = contact + "|" + $scope.user;
             }
             Pubnub.subscribe({
@@ -47,7 +49,7 @@ angular.module('myApp.chat', ['ngRoute',"pubnub.angular.service"])
             });
             Pubnub.history({
                 channel: $scope.channel,
-                callback: function(m) {
+                callback: function (m) {
                     console.log(m);
                     $scope.currentMessages.length = 0;
                     angular.extend($scope.currentMessages, m[0]);
@@ -56,16 +58,16 @@ angular.module('myApp.chat', ['ngRoute',"pubnub.angular.service"])
             });
         }
 
-        $scope.initChat = function(){
+        $scope.initChat = function () {
             Pubnub.init({
                 publish_key: 'pub-c-50877502-ae22-45dc-a606-d8f589fa76db',
                 subscribe_key: 'sub-c-157f2f30-7c0e-11e9-a950-f249fab64e16',
                 uuid: $scope.user
             });
-            $scope.changeContact($scope.contacts[0]);
+            //$scope.changeContact($scope.contacts[0].Username);
         }
 
-        
+
         $scope.channel = "";
         $scope.contacts = [];
         $scope.currentMessages = [];
@@ -97,15 +99,15 @@ angular.module('myApp.chat', ['ngRoute',"pubnub.angular.service"])
                         };
                         $http(req).then(function successCallback(response) {
                             response.data.Items.forEach(element => {
-                                $scope.contacts.push(element.Datos.Username);
                                 var chan = $scope.user + "|" + element.Datos.Username;
+                                $scope.contacts.push(element.Datos);
                             });
                             $scope.initChat();
                         }, function errorCallback(response) {
                             console.error(response);
                         });
 
-                        }
+                    }
                     else {
                         window.location.href = "#!/info";
                     }
@@ -120,6 +122,5 @@ angular.module('myApp.chat', ['ngRoute',"pubnub.angular.service"])
             Swal.fire(error);
             window.location.href = '#!/login';
         });
-        
+
     }]);
-    
